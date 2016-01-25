12f2426fc0acea86ed84d8962084f57f0a4de6a2
Nathan Pollon <nathanpollon@gmail.com>
2016-01-24 21:51:36 -0800

    Cleaned up code,  removed unnecessary opcodes
    Removed all giant comment walls, cleaned up to make more readable.

--- AutoOp.java
+++ AutoOp.java
@@ -1,68 +1,51 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
-
-import com.qualcomm.robotcore.*;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
-/**
- * Auto Mode
- * <p>
- * Enables the bot to move by its self
- */
 
 public class AutoOp extends LinearOpMode {
 
-        DcMotor motorRight;
-        DcMotor motorLeft;
-        DcMotor backRight;
-        DcMotor backLeft;
-        DcMotor armExtend;
-        DcMotor armAdjust;
-        Servo wingRight;
-        Servo wingLeft;
-        Servo bdLeft;
-        Servo bdRight;
-        Servo dumpRotate;
-        Servo dumpExtend;
+        DcMotor motorRight, motorLeft, backRight, backLeft, armExtend, armAdjust;
+        Servo wingRight, wingLeft, bdLeft, bdRight, dumpRotate, dumpExtend;
 
     @Override
     public void runOpMode() throws InterruptedException {
 
-
         motorRight = hardwareMap.dcMotor.get("motorRight");
         motorLeft = hardwareMap.dcMotor.get("motorLeft");
+        motorLeft.setDirection(DcMotor.Direction.REVERSE);
+
         backRight = hardwareMap.dcMotor.get("backRight");
         backLeft = hardwareMap.dcMotor.get("backLeft");
         backLeft.setDirection(DcMotor.Direction.REVERSE);
-        motorLeft.setDirection(DcMotor.Direction.REVERSE);
+
         armExtend = hardwareMap.dcMotor.get("armExtend");
         armAdjust = hardwareMap.dcMotor.get("armAdjust");
+
         wingRight = hardwareMap.servo.get("wingRight");
         wingLeft = hardwareMap.servo.get("wingLeft");
-        bdLeft = hardwareMap.servo.get("bdLeft");/*bd is */
+
+        bdLeft = hardwareMap.servo.get("bdLeft");
         bdRight = hardwareMap.servo.get("bdRight");
+
         dumpRotate = hardwareMap.servo.get("dumpRotate");
         dumpExtend = hardwareMap.servo.get("dumpExtend");
 
-        double s = .2;
+        final double s = .2; // general motor speed
 
         waitForStart();
 
         long startTime = System.currentTimeMillis();
+
 
         motorRight.setPower(s);
         motorLeft.setPower(s);
         backLeft.setPower(s);
         backRight.setPower(s);
 
-
-        while(System.currentTimeMillis() <= startTime+5350){
-
-        }
+        while(System.currentTimeMillis() <= startTime+5350){} // 6.5 ft
 
         motorRight.setPower(0);
         motorLeft.setPower(0);
